7f4447e2f99a9792604ae298552e5add68bdc0ac
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-16 21:01:19 +0100

    Change page count to Integer

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -3,18 +3,18 @@ window.addEventListener("load", function (e) {
   populateStorage();
   render();
 });
+
 function populateStorage() {
   if (myLibrary.length == 0) {
-    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
+    let book1 = new Book("Robison Crusoe", "Daniel Defoe", 252, true);
     let book2 = new Book(
       "The Old Man and the Sea",
       "Ernest Hemingway",
-      "127",
+      127,
       true
     );
     myLibrary.push(book1);
     myLibrary.push(book2);
-    render();
   }
 }
 const title = document.getElementById("title");
@@ -26,18 +26,16 @@ const addBookBtn = document.getElementById("addBook");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function addBook() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    author.value == null ||
-    author.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
+  if (title.value == "" || author.value == "" || pages.value == "") {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
+    let book = new Book(
+      title.value,
+      author.value,
+      Number(pages.value),
+      check.checked
+    );
     myLibrary.push(book);
     render();
   }
